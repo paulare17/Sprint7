d141a0d4744ae9a39fad1ff89ccab43b545b3bdb
Paula <paulare17@gmail.com>
2025-05-26 19:06:57 +0200

    Actualitzar DOM

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,46 +1,23 @@
-import React from "react";
-import { useEffect, useState } from "react";
+//redux
+import { useGetDadesQuery} from '../../features/apiSlice'
+import type { Film } from '../../features/apiSlice'
 
-interface Film {
-  original_title: string;
-  poster_path: string;
-  overview: string;
+
+export default function List() {
+  const { data, isLoading, isError } = useGetDadesQuery()
+
+  if (isLoading) return <p>Carregant...</p>
+  if (isError) return <p>Error en carregar la llista de pel·lícules</p>
+  if (!data.results || data.results.length === 0) return <p>No hi ha pel·lícules disponibles</p>;
+
+  console.log(data.results)
+
+  return (
+    <div>
+      {data.results.map((movie:Film) => (
+        <img key={movie.id} src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}/>
+      ))}
+    </div>
+  )
 }
 
-const List: React.FC = () => {
-  const [films, setFilms] = useState<Film[]>([]);
-
-  useEffect(() => {
-    const apiKey = "38aec9dcc915b82585b7be878fba2d4b";
-    const page = 1; // veure com arreglo lo de les pgs
-
-    fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.results[0]); // objecte JSON
-        setFilms(data.results);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }, []);
-
-  const film = () => {
-    return films.map((film) => (
-      <div className="element">
-        <img src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} />{" "}
-        <br></br>
-        <h4>{film.original_title}</h4> <br></br>
-        <h5>
-          {film.overview} <br></br>
-        </h5>
-      </div>
-    ));
-  };
-
-  return <section>{film()}</section>;
-};
-
-export default List;
